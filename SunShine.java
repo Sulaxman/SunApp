2b166d62f9783b0a948308e944fde4ce466dbcb7
sulaxman <sulaxmank@gmail.com>
2017-03-30 15:04:49 +0530

    Devops training

--- SunShine.java
+++ SunShine.java
@@ -1,17 +1,16 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
-	import org.junit.Before;
-	import org.openqa.selenium.By;
-	import org.openqa.selenium.Point;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 	public class SunShine
 	{
-	   static WebDriver driver;
+		
+	  static WebDriver driver;
 	   @Before
 	   public void OpenBrowser()
 		 {
@@ -21,7 +20,7 @@ import org.junit.Test;
 	   @Test
 	   public void Open()
 	   {
-			driver.get("http://google.co.in");
+		   	driver.get("http://google.co.in");
 			Point point=driver.findElement(By.id("hplogo")).getLocation();
 			driver.findElement(By.name("q")).sendKeys("test");
 			driver.findElement(By.name("btnG")).click();
