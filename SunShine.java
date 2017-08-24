f3df18f253cfb8b799fd9c640b74cee8d5d712dc
Suhasan Kaja <sulaxmank@gmail.com>
2017-08-24 16:06:08 +0530

    Second Commit for project training BMW

--- SunShine.java
+++ SunShine.java
@@ -8,22 +8,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 	public class SunShine
 	{
 		
-	//  static WebDriver driver;
-		File file = new File("C://Selenium//IEDriverServer_x64_3.4.0//IEDriverServer.exe");
-		System.setproperty("webdriver.ie.driver", file.getAbsolutePath()) ;
-			static WebDriver driver;
+	  static WebDriver driver;
 		 
 	   // Writing before annotation so that the method run before execution of any other method
 	   @Before
 	   public void OpenBrowser()
 		 {
-		//	driver=new FirefoxDriver();
-		   driver = new InternetExplorerDriver();			
+			driver=new FirefoxDriver();
+					
 		 }
 	   // Writing test annotation to convert the open method as a test condition
 	   @Test
@@ -32,7 +28,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 		   	driver.get("http://google.co.in");
 			Point point=driver.findElement(By.id("hplogo")).getLocation();
 			driver.findElement(By.name("q")).sendKeys("sulaxman");
-			driver.findElement(By.name("btnG")).click();
+			driver.findElement(By.name("btnK")).click();
 			driver.navigate().refresh();
 			driver.close();
 		}
